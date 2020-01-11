85c90556e9a4f45b657c0c8402c3aa578c28f315
zvina <zvinaor@gmail.com>
2020-01-11 20:04:43 +0200

    last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  color: boolean;
+  color: boolean = true;
 
   getColor(event) {
     this.color = event;
