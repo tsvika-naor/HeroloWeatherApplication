ea5f1dd16aaf6549e44e11a3a9b23d868982b385
zvina <zvinaor@gmail.com>
2020-01-23 19:35:29 +0200

    last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  color: boolean = true;
-
-  getColor(event) {
-    this.color = event;
-  }
-  getColors(){
-
-  }
+export class AppComponent implements OnInit{
+constructor(private renderer: Renderer2) {
+  this.renderer.addClass(document.body, 'body-class');
 }
+
+    color: boolean = true;
+
+    getColor(event) {
+      this.color = event;
+      let tempColor = 'rgb(0,70,130)';
+      if(this.color=== false)
+        tempColor = 'lightslategrey';
+      this.renderer.setStyle(document.body,'backgroundColor',tempColor);
+    }
+
+    getColors() {
+
+    }
+
+  ngOnInit(): void {
+    this.renderer.setStyle(document.body,'backgroundColor', 'rgb(0,70,130)');
+
+  }
+  }
